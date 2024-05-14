e16ee04cc74ef14a05ff2d5c61eb7ea126ed5b34
maninisheth <maninisheth@gmail.com>
2024-05-14 08:54:58 +0100

    homeworkdone

---
+++ src/homework_week_8/Programme_24_method_overriding/ICICI.java
@@ -0,0 +1,9 @@
+package homework_week_8.Programme_24_method_overriding;
+
+public class ICICI extends Bank {
+    public int getRateOfInterest() {
+        return 7;
+    }
+}
+
+
---
+++ src/homework_week_8/Programme_24_method_overriding/SBI.java
@@ -0,0 +1,10 @@
+package homework_week_8.Programme_24_method_overriding;
+/**
+ * Creating child classes.
+ */
+public class SBI {
+    public int getRateOfInterest() {
+        return 8;
+    }
+
+}
